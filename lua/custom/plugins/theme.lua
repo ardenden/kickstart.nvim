13f1dc71163e133798bea627fbf5a8c201ac26c7
arden <ardenhilario@gmail.com>
2024-07-23 19:42:47 +0800

    theme config

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,6 +1,18 @@
 return {
     "aktersnurra/no-clown-fiesta.nvim",
     config = function()
+        require("no-clown-fiesta").setup({
+            transparent = false, -- Enable this to disable the bg color
+            styles = {
+                comments = { italic = true },
+                functions = {},
+                keywords = {},
+                lsp = { underline = true },
+                match_paren = {},
+                type = { bold = false },
+                variables = {},
+            },
+        })
         vim.cmd([[colorscheme no-clown-fiesta]])
     end,
 }
